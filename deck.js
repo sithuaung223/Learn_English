d754464f3d7d70fbbef3c6e72eb03945105fc40c
Si Thu Aung <sithuaung223@gmail.com>
2018-05-03 17:42:48 -0500

    clean document and refactor code for filtering and displaying card

--- deck.js
+++ deck.js
@@ -1,4 +1,4 @@
-function createCardBtn(doc, dict, w, flash_card_dict) {
+function createCardBtn(dict, w, flash_card_dict) {
 	var card = {
 		front_side: w,
 		flipped_side: dict[w],
@@ -13,10 +13,10 @@ function createCardBtn(doc, dict, w, flash_card_dict) {
 	return card_btn;
 }
 
-function createDeckWithDictionary(doc, dict, flash_card_dict) {
+function createDeckWithDictionary(dict, flash_card_dict) {
 	var deck = [];
 	for (var w in dict) {
-		var card_btn = createCardBtn(doc, dict, w, flash_card_dict);
+		var card_btn = createCardBtn(dict, w, flash_card_dict);
 		deck.push(card_btn);
 	}
 	return deck;
@@ -26,30 +26,29 @@ function getIndexOfCard(cards, current_card_btn) {
 	return index = cards.indexOf(current_card_btn);
 }
 
-function insertNewCard(doc, new_card, next_btn) {
+function insertNewCard(new_card, next_btn) {
 	if (new_card != null ) {
-		doc.body.insertBefore(new_card, next_btn);
+		document.body.insertBefore(new_card, next_btn);
 	}
 }
 
-// function getNewCardBtn(deck, current_card_btn, btn) {
-// 	var index = getIndexOfCard(deck.cards, current_card_btn);
-// 	var new_card_btn = (btn.current.id =='next') 
-// 					? deck.cards[index+1]
-// 					: deck.cards[index-1];
-// 	return new_card_btn;
-// }
+function getDisplayDeckWithIsFiltered(deck, filtered_deck, isFiltered) {
+	var display_deck = (isFiltered) ? filtered_deck : deck;
+	return display_deck;
+}
 
-function replaceWithNewCardBtn(doc, deck, current_card_btn, btn, isLearned) {
-	flash_card_dict[current_card_btn.id].card.isLearned = isLearned;
-
+function getNewCardBtn(deck, current_card_btn, btn) {
 	var index = getIndexOfCard(deck.cards, current_card_btn);
 	var new_card_btn = (btn.current.id =='next') 
 					? deck.cards[index+1]
 					: deck.cards[index-1];
-	// var new_card_btn = getNewCardBtn(deck, current_card_btn, btn);
+	return new_card_btn;
+}
+
+function replaceWithNewCardBtn(current_card_btn, new_card_btn, next_btn, isLearned) {
 	document.body.removeChild(current_card_btn);
-	insertNewCard(doc, new_card_btn, btn.next);
+	insertNewCard(new_card_btn, next_btn);
+   	isLearned.checked = cardBtnIsLearned(new_card_btn);
 
 	return new_card_btn;
 }
@@ -58,7 +57,7 @@ function cardBtnIsLearned(card_btn) {
 	return flash_card_dict[ card_btn.id ].card.isLearned;
 }
 
-function getFilteredDeck(deck, filtered_deck) {
+function updateFilteredDeck(deck, filtered_deck) {
     	filtered_deck.cards = [];
     	for (var i in deck.cards) {
     		var card_btn = deck.cards[i];
