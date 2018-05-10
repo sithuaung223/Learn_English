e83cd6823b26fb5801c4c495bc90d88a498d0dcf
Si Thu Aung <sithuaung223@gmail.com>
2018-05-10 14:26:54 -0500

    add detail page for category, pass Json from view to template : create python dict from Query set, dumps as Json, pass it through render to template view and extract as javascript dict from json with either | safe or JSON.parse {{ | escape}}

---
+++ learn_english/categories/static/categories/deck.js
@@ -0,0 +1,76 @@
+function createCardBtn(dict, w, flash_card_dict) {
+	var card = {
+		front_side: w,
+		flipped_side: dict[w],
+		isLearned: false,
+		dateIsLearned: null,
+		remindingDayCount: 1 
+	};
+	var flash_card = {
+		card: card,
+		isFrontSide: true
+	}
+	var card_btn = createCardBtnWithFlashCard(w, flash_card);
+	flash_card_dict[w] = flash_card;
+	return card_btn;
+}
+
+function createDeckWithDictionary(dict, flash_card_dict) {
+	var deck = [];
+	for (var w in dict) {
+		var card_btn = createCardBtn(dict, w, flash_card_dict);
+		deck.push(card_btn);
+	}
+	return deck;
+}
+
+function getIndexOfCard(cards, current_card_btn) {
+	return index = cards.indexOf(current_card_btn);
+}
+
+function insertNewCard(new_card, next_btn) {
+	if (new_card != null ) {
+		document.body.insertBefore(new_card, next_btn);
+	}
+}
+
+function getDisplayDeckWithIsFiltered(deck, filtered_deck, isFiltered) {
+	filtered_deck = updateFilteredDeck(deck, filtered_deck);
+	var display_deck = (isFiltered) ? filtered_deck : deck;
+	return display_deck;
+}
+
+function getNewCardBtn(cards, current_card_btn, btn) {
+	var index = getIndexOfCard(cards, current_card_btn);
+	var new_card_btn = (btn.id =='next') 
+					? cards[index+1]
+					: cards[index-1];
+	return new_card_btn;
+}
+
+function replaceWithNewCardBtn(current_card_btn, new_card_btn, next_btn) {
+	document.body.removeChild(current_card_btn);
+	insertNewCard(new_card_btn, next_btn);
+	return new_card_btn;
+}
+
+function cardBtnIsLearned(card_btn) {
+	return flash_card_dict[ card_btn.id ].card.isLearned;
+}
+
+function updateFilteredDeck(deck, filtered_deck) {
+	filtered_deck.cards = [];
+	for (var i in deck.cards) {
+		var card_btn = deck.cards[i];
+		if (cardBtnIsLearned(card_btn))
+			filtered_deck.cards.push(card_btn);
+	}
+	return filtered_deck;
+}
+
+function isEndDisableButton(cards, current_card_btn, next_btn, prev_btn) {
+   	var index = getIndexOfCard(cards, current_card_btn);
+   	prev_btn.disabled = (index <= 0 ) ? true : false;
+   	next_btn.disabled = (index >= cards.length-1) ? true : false;
+}
+
--- learn_english/categories/views.py
+++ learn_english/categories/views.py
@@ -2,6 +2,7 @@
 from django.http import HttpResponse
 from django.template import loader
 from .models import Category, Vocabulary
+import json
 
 def index(request):
     category_list = Category.objects.all();
@@ -15,7 +16,12 @@ def detail(request, category_id):
     template = loader.get_template('categories/detail.html')
     selected_category_obj = Category.objects.get(id = category_id)
     deck = Vocabulary.objects.filter(category=selected_category_obj)
+
+    meaing_dict = {}
+    for card in deck:
+        meaing_dict[card.name] = card.meaning
+
     context = {
-        'deck' : deck,
+        'meaning_dict' : json.dumps(meaing_dict),
     }
     return HttpResponse(template.render(context, request))
