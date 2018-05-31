c812249f2ef0daeace8a5ca7c96447795f78fe54
Si Thu Aung <sithuaung223@gmail.com>
2018-05-30 21:27:49 -0500

    added functioning reminder page, available for testing

--- learn_english/categories/urls.py
+++ learn_english/categories/urls.py
@@ -5,5 +5,7 @@ app_name = 'categories'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:category_id>', views.detail, name='detail'),
+    path('<int:category_id>/reminder', views.reminder, name='reminder'),
     path('ajax/update_isLearnedCard', views.update_isLearnedCard, name='update_isLearnedCard'),
+    path('ajax/update_rememberedCard', views.update_rememberedCard, name='update_rememberedCard'),
 ]
--- learn_english/categories/views.py
+++ learn_english/categories/views.py
@@ -6,6 +6,8 @@ import json
 from django.core import serializers
 from django.http import JsonResponse
 from django.utils import timezone
+import datetime
+import math
 
 def index(request):
     category_list = Category.objects.all();
@@ -32,6 +34,31 @@ def detail(request, category_id):
     }
     return render(request, 'categories/detail.html', context)
 
+
+def reminder(request, category_id):
+    selected_category_obj = Category.objects.get(id = category_id)
+    vocabularies = Vocabulary.objects.filter(category=selected_category_obj)
+
+    meaning_dict = {}
+    remind_card_dict = {}
+    for vocab in vocabularies:
+        card = Card.objects.get(front_side=vocab.name)
+        today = timezone.localtime(timezone.now()).date()
+        # reminderDate = card.dateIsLearned + datetime.timedelta(days=card.remindingDayCount)
+        # TODO: testing, NEED to change back to above line for real prototype
+        reminderDate = card.dateIsLearned + datetime.timedelta(days=0)
+        if (card.isLearned and (reminderDate <= today)):
+            meaning_dict[vocab.name] = vocab.meaning
+            reminder_card_data = serializers.serialize("json", [card])
+            remind_card_dict[vocab.name] = reminder_card_data
+
+    context = {
+        'meaning_dict': meaning_dict,
+        'remind_card_dict': remind_card_dict,
+    }
+    return render(request, 'categories/reminder.html',context)
+
+
 def update_isLearnedCard(request):
     front_side = request.GET.get('front_side', None)
     isLearned = request.GET.get('isLearned', None)
@@ -43,8 +70,9 @@ def update_isLearnedCard(request):
     local_time = timezone.localtime(timezone.now())
     local_date = local_time.date()
     card.dateIsLearned = local_date
+    card.remindingDayCount = 1
 
-    card.save(update_fields = ["isLearned", "dateIsLearned"])
+    card.save(update_fields = ["isLearned", "dateIsLearned", "remindingDayCount"])
 
     data = {
         'name': card.front_side,
@@ -52,3 +80,25 @@ def update_isLearnedCard(request):
         'dateIsLearned': card.dateIsLearned,
     }
     return JsonResponse(data)
+
+def update_rememberedCard(request):
+    front_side = request.GET.get('front_side', None)
+    isRemembered = request.GET.get('isRemembered', None)
+
+    card = Card.objects.get(front_side = front_side)
+    if (isRemembered == 'true'):
+        card.remindingDayCount *= 2
+    else:
+        card.remindingDayCount *= 1/2
+
+    card.remindingDayCount = math.ceil(card.remindingDayCount)
+    card.save(update_fields = ["remindingDayCount"])
+
+    data = {
+        'name': card.front_side,
+        'dateIsLearned': card.dateIsLearned,
+        'remindingDayCount': card.remindingDayCount,
+        'isRemembered': isRemembered,
+    }
+    return JsonResponse(data)
+
